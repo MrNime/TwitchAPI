01a7483f0ce43d651fb533c41fe0952c6bf1e646
Nicky Meuleman <nicky.meuleman@gmail.com>
2017-09-05 20:00:22 +0200

    functional

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     console.log('ready');
     var api = "https://wind-bow.gomix.me/twitch-api";
-    var users = ["ESL_SC2", "freecodecamp"];
+    var users = ["ESL_SC2", "freecodecamp", "Northernlion", "last_grey_wolf", "nonexistant_channelsdfsdf"];
     // var userIDs = [26177965760, ];
 
     users.forEach(function(user) {
@@ -12,25 +12,74 @@ $(document).ready(function() {
             dataType: 'jsonp',
             success: function(data) {
                 console.log(data);
-                var logo = data.logo;
-                var display_name = data.display_name;
-                //get stream info
-
-                //make card
-                var html =
-                `            <div class="card">
-                <div class="card-body">
-                <div class="avatar">
-                <img src=${logo} alt="avatar">
-                </div>
-                <div class="cardInfo">
-                <div class="title">${display_name}</div>
-                <div class="info">currently playing: a game</div>
-                </div>
-                </div>
-                </div>`
-                //display card
-                $('#cardBox').append(html);
+                var logo = data.logo || 'http://via.placeholder.com/100x100';
+                var display_name = data.display_name || user;
+                if (data.error) {
+                    html =
+                    `
+                    <div class="card">
+                    <div class="card-body">
+                    <div class="avatar">
+                    <img src=${logo} alt="avatar">
+                    </div>
+                    <div class="cardInfo">
+                    <div class="title">Channel does not exist</div>
+                    </div>
+                    </div>
+                    </div>
+                    `;
+                    $('#cardBox').append(html);
+                } else {
+                    //get stream info
+                    $.ajax({
+                        type: 'GET',
+                        url: api + '/streams/' + user + '?callback=?',
+                        dataType: 'jsonp',
+                        success: function(data) {
+                            console.log(data);
+                            if (data.stream === null) {
+                                html =
+                                `
+                                <a href="https://www.twitch.tv/${user}" target="_blank">
+                                <div class="card">
+                                <div class="card-body">
+                                <div class="avatar">
+                                <img src=${logo} alt="avatar">
+                                </div>
+                                <div class="cardInfo">
+                                <div class="title">${display_name}</div>
+                                <div class="info">Offline</div>
+                                </div>
+                                </div>
+                                </div>
+                                </a>
+                                `;
+                                $('#cardBox').append(html);
+                            } else {
+                                var game = data.stream.game;
+                                //make card
+                                var html =
+                                `
+                                <a href="https://www.twitch.tv/${user}" target="_blank">
+                                <div class="card">
+                                <div class="card-body">
+                                <div class="avatar">
+                                <img src=${logo} alt="avatar">
+                                </div>
+                                <div class="cardInfo">
+                                <div class="title">${display_name}</div>
+                                <div class="info">currently playing: ${game || ""}</div>
+                                </div>
+                                </div>
+                                </div>
+                                </a>
+                                `;
+                                //display card
+                                $('#cardBox').prepend(html);
+                            }
+                        }
+                    })
+                }
             }
         })
     });
